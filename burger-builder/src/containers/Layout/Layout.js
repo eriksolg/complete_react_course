5fd736d6153c3e6d396fd5312010b669e1ccd33f
eriksol <erik.solg@tallink.ee>
2021-01-24 03:52:24 +0200

    Created components

---
+++ burger-builder/src/containers/Layout/Layout.js
@@ -0,0 +1,46 @@
+import React, { Fragment, Component } from 'react';
+
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevstate) =>
+            {
+                return {
+                    showSideDrawer: !prevstate.showSideDrawer
+                }
+            }
+        )
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    }
+
+};
+
+export default Layout;
